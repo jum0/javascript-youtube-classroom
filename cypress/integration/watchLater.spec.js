694874760395e62f98e94b2999c3cd6bb6b93b13
bigsaigon333 <likepeppermint@gmail.com>
2021-03-10 20:01:21 +0900

    test: 검색된 영상을 저장하면, 해당 영상이 볼 영상 화면에 표시되는 것을 테스트한다

--- cypress/integration/watchLater.spec.js
+++ cypress/integration/watchLater.spec.js
@@ -20,6 +20,13 @@ describe("볼 영상 화면을 테스트한다.", () => {
     }).as("searchFromScroll");
   });
 
+  const search = (keyword) => {
+    cy.get(`.${CLASSNAME.SEARCH_TAB}`).click();
+    cy.get(`.${CLASSNAME.SEARCH_FORM_INPUT}`).type(keyword);
+    cy.get(`.${CLASSNAME.SEARCH_FORM_BUTTON}`).click();
+    cy.wait("@searchFromKeyword");
+  };
+
   it("가장 처음에 저장된 영상이 없는 경우, 비어있다는 것을 사용자에게 알려준다,", () => {
     cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("be.visible");
   });
@@ -27,12 +34,37 @@ describe("볼 영상 화면을 테스트한다.", () => {
   it("저장된 영상이 있는 경우, 비어있지 않다는 것을 사용자에게 알려준다,", () => {
     const keyword = "배민";
 
-    cy.get(`.${CLASSNAME.VIDEO_SEARCH_TAB}`).click();
-    cy.get(`.${CLASSNAME.YOUTUBE_SEARCH_FORM_INPUT}`).type(keyword);
-    cy.get(`.${CLASSNAME.YOUTUBE_SEARCH_FORM_BUTTON}`).click();
+    cy.get(`.${CLASSNAME.SEARCH_TAB}`).click();
+    cy.get(`.${CLASSNAME.SEARCH_FORM_INPUT}`).type(keyword);
+    cy.get(`.${CLASSNAME.SEARCH_FORM_BUTTON}`).click();
     cy.wait("@searchFromKeyword");
 
     cy.get(`.${CLASSNAME.SAVE_VIDEO_BUTTON}`).first().click();
     cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("not.be.visible");
+  });
+
+  it("검색된 영상을 저장하면, 해당 영상이 볼 영상 화면에 표시된다.", () => {
+    const keyword = "주주주";
+    search(keyword);
+
+    let savedVideoId;
+
+    cy.get(`.${CLASSNAME.SAVE_VIDEO_BUTTON}`)
+      .first()
+      .click()
+      .invoke("attr", "data-video-id")
+      .then((videoId) => {
+        savedVideoId = videoId;
+      });
+
+    cy.get(`.${CLASSNAME.MODAL_CLOSE}`).click();
+    // cy.wait(3000);
+    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("not.be.visible");
+    cy.get(`.${CLASSNAME.WATCH_LATER_VIDEO_WRAPPER}`)
+      .find("iframe")
+      .invoke("attr", "src")
+      .then((src) => {
+        expect(src).to.match(new RegExp(`${savedVideoId}$`));
+      });
   });
 });
